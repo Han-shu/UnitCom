6b0f773b51e503bc596733a0361f51a1c0308cf9
Han-shu <1051565276@qq.com>
2024-02-27 11:50:19 -0500

    modified:   src/gen_info_extraction.jl 	new file:   src/tutorial_1.jl 	new file:   src/tutorial_2.jl

--- src/gen_info_extraction.jl
+++ src/gen_info_extraction.jl
@@ -1,5 +1,7 @@
 using PowerSystems, Gurobi, JuMP
 using PowerSystemCaseBuilder
+using Dates
+using TimeSeries
 
 sys = System("/Users/hanshu/Desktop/Price_formation/Data/Doubleday_data/DA_sys_31_scenarios.json")
 
@@ -7,6 +9,37 @@ get_components(ThermalMultiStart, sys)
 get_components(RenewableDispatch, sys)
 thermal_gen_names = get_name.(get_components(ThermalGen, sys))
 renewable_gen_names = get_name.(get_components(RenewableGen, sys))
-uc_sys = System("/Users/hanshu/Desktop/Price_formation/Data/Doubleday_data/HA_sys_UC_experiment.json")
+
+thermal_gen_names2 = get_name.(get_components(ThermalMultiStart, sys))
+renewable_gen_names2 = get_name.(get_components(RenewableDispatch, sys))
 
 
+file_dir = joinpath(pkgdir(PowerSystems), "docs", "src", "tutorials", "tutorials_data")
+system = System(joinpath(file_dir, "RTS_GMLC.m"));
+to_json(system, "system.json")
+system2 = System("system.json")
+
+
+resolution = Dates.Hour(1)
+data = Dict(
+    DateTime("2020-01-01T00:00:00") => 10.0*ones(24),
+    DateTime("2020-01-01T01:00:00") => 5.0*ones(24),
+)
+forecast = Deterministic("max_active_power", data, resolution)
+
+
+resolution = Dates.Hour(1)
+dates = range(DateTime("2020-01-01T00:00:00"), step = resolution, length = 24)
+data = TimeArray(dates, ones(24))
+time_series = SingleTimeSeries("max_active_power", data)
+
+
+# Create static time series data.
+resolution = Dates.Hour(1)
+dates = range(DateTime("2020-01-01T00:00:00"), step = resolution, length = 8760)
+data = TimeArray(dates, ones(8760))
+ts = SingleTimeSeries("max_active_power", data)
+add_time_series!(sys, component, ts)
+
+# Transform it to Deterministic
+transform_single_time_series!(sys, 24, Hour(24))
---
+++ src/tutorial_1.jl
@@ -0,0 +1,42 @@
+using PowerSystems
+using PowerSimulations
+using HydroPowerSimulations
+using PowerSystemCaseBuilder
+using Gurobi # solver
+
+# data
+sys = build_system(PSISystems, "modified_RTS_GMLC_DA_sys")
+# load an empty template
+template_uc = ProblemTemplate()
+# Add branch
+set_device_model!(template_uc, Line, StaticBranch)
+set_device_model!(template_uc, Transformer2W, StaticBranch)
+set_device_model!(template_uc, TapTransformer, StaticBranch)
+# Add injecrtion devices
+set_device_model!(template_uc, ThermalStandard, ThermalStandardUnitCommitment)
+set_device_model!(template_uc, RenewableDispatch, RenewableFullDispatch)
+set_device_model!(template_uc, PowerLoad, StaticPowerLoad)
+set_device_model!(template_uc, HydroDispatch, HydroDispatchRunOfRiver)
+set_device_model!(template_uc, RenewableFix, FixedOutput)
+
+# Add reserve
+set_service_model!(template_uc, VariableReserve{ReserveUp}, RangeReserve)
+set_service_model!(template_uc, VariableReserve{ReserveDown}, RangeReserve)
+# network
+set_network_model!(template_uc, NetworkModel(CopperPlatePowerModel))
+# solver
+solver = optimizer_with_attributes(Gurobi.Optimizer, "MIPGap" => 0.5)
+# solver = optimizer_with_attributes(HiGHS.Optimizer, "mip_rel_gap" => 0.5)
+
+# Build an DecisionModel
+problem = DecisionModel(template_uc, sys; optimizer = solver, horizon = 24)
+build!(problem, output_dir = mktempdir())
+
+# Solve an DecisionModel
+solve!(problem)
+# Results inspection
+res = ProblemResults(problem)
+get_optimizer_stats(res)
+read_variables(res)
+list_parameter_names(res)
+read_parameter(res, "ActivePowerTimeSeriesParameter__RenewableDispatch")
