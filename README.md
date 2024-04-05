e210fcbb90b3d4c10ca35a2fcf6c85ee3ff91180
Han-shu <1051565276@qq.com>
2024-04-05 11:44:21 -0400

    modified:   README.md 	modified:   system/NYGrid/add_ts.jl 	modified:   system/case5_re.jl

--- README.md
+++ README.md
@@ -12,16 +12,14 @@
 # Time series TODO
 - Generate time series with NYISO data
 - Make sure the first time point is the same for all scenarios
-- Normalize the time series data
+- ~~Normalize the time series data (divide by the base power)~~
 
 # NY Grid TODO
 - [NY_Sienna_Conversion](https://github.com/gackermannlogan/NY_Sienna_Conversion)
-- Thermal Generators operation cost (start-up, shut-down, no-load cost)
-- Batteries
-- Time series data (wind, solar, load, gas price)
-
-# Questions
-- Generation cost: Heat rate, TwoPartCost, ThreePartCost, MarketBids?
+- Thermal Generators operation cost 
+    - Heat rate, TwoPartCost, ThreePartCost, MarketBids?
     - ThreePartCost: Fixed cost, variable cost, and start-up cost
-- [ThermalStandard](https://nrel-sienna.github.io/PowerSystems.jl/stable/model_library/generated_ThermalStandard/#ThermalStandard)
-
+    - Need: start-up, shut-down, no-load cost
+    - heat rate * fuel cost
+- ~~Battery~~
+- ~~Time series data (wind, solar, load)~~
--- system/NYGrid/add_ts.jl
+++ system/NYGrid/add_ts.jl
@@ -1,15 +1,10 @@
-using PowerSystems, PowerSimulations, HydroPowerSimulations
+using PowerSystems
 using JSON3, Dates, HDF5, Statistics
-const PSI = PowerSimulations
-const PSY = PowerSystems
 
 ts_dir = "/Users/hanshu/Desktop/Price_formation/Data/generate_fr_KBoot/idx_hour"
 solar_file = joinpath(ts_dir, "solar_scenarios.h5")
 wind_file = joinpath(ts_dir, "wind_scenarios.h5")
 load_file = joinpath(ts_dir, "load_scenarios.h5")
-
-# file_dir = joinpath(pkgdir(PowerSystems), "docs", "src", "tutorials", "tutorials_data")
-# system = System(joinpath(file_dir, "case5_re.m"), assign_new_uuids = true)
 
 thermal_gen_names = get_name.(get_components(ThermalGen, system))
 renewable_gen_names = get_name.(get_components(RenewableGen, system))
@@ -32,64 +27,66 @@ solar_gens = get_components(
 
 
 initial_time = Dates.DateTime(2018, 1, 1)
-da_interval = Dates.Hour(24)
 da_resolution = Dates.Hour(1)
 hour_count = 8760-48+1
+scenario_count = 10
+base_power = PSY.get_base_power(system)
+
 hour_ahead_forecast = Dict{Dates.DateTime, Matrix{Float64}}()
-
-
 for ix in 1:hour_count
     curr_time = initial_time + Hour(ix - 1)
     forecast = h5open(solar_file, "r") do file
         return read(file, string(curr_time))
     end
     forecast[1, :] .= mean(forecast[1, :])
-    hour_ahead_forecast[curr_time] = forecast
+    hour_ahead_forecast[curr_time] = forecast./base_power
 end
 
 scenario_forecast_data = Scenarios(
     name = "solar_power",
     resolution = da_resolution,
     data = hour_ahead_forecast,
-    scenario_count = 10,
-    scaling_factor_multiplier = PSY.get_max_active_power
+    scenario_count = scenario_count,
+    scaling_factor_multiplier = PSY.get_base_power
 )
 add_time_series!(system, solar_gens, scenario_forecast_data)
 
 
+hour_ahead_forecast = Dict{Dates.DateTime, Matrix{Float64}}()
 for ix in 1:hour_count
     curr_time = initial_time + Hour(ix - 1)
     forecast = h5open(wind_file, "r") do file
         return read(file, string(curr_time))
     end
     forecast[1, :] .= mean(forecast[1, :])
-    hour_ahead_forecast[curr_time] = forecast
+    hour_ahead_forecast[curr_time] = forecast./base_power
 end
 
 scenario_forecast_data = Scenarios(
     name = "wind_power",
     resolution = da_resolution,
     data = hour_ahead_forecast,
-    scenario_count = 10,
-    scaling_factor_multiplier = PSY.get_max_active_power
+    scenario_count = scenario_count,
+    scaling_factor_multiplier = PSY.get_base_power
 )
 add_time_series!(system, wind_gens, scenario_forecast_data)
 
 
+hour_ahead_forecast = Dict{Dates.DateTime, Matrix{Float64}}()
 for ix in 1:hour_count
     curr_time = initial_time + Hour(ix - 1)
     forecast = h5open(load_file, "r") do file
         return read(file, string(curr_time))
     end
     forecast[1, :] .= mean(forecast[1, :])
-    hour_ahead_forecast[curr_time] = forecast
+    hour_ahead_forecast[curr_time] = forecast./base_power
 end
 
 scenario_forecast_data = Scenarios(
     name = "load",
     resolution = da_resolution,
     data = hour_ahead_forecast,
-    scenario_count = 10,
-    scaling_factor_multiplier = PSY.get_max_active_power
+    scenario_count = scenario_count,
+    scaling_factor_multiplier = PSY.get_base_power
 )
 add_time_series!(system, loads, scenario_forecast_data)
