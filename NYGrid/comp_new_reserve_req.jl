b64a416f8d005b67d375c948146b1c68864fd036
hanshu <1051565276@qq.com>
2024-09-28 23:51:46 -0400

    modified:   NYGrid/comp_new_reserve_req.jl

--- NYGrid/comp_new_reserve_req.jl
+++ NYGrid/comp_new_reserve_req.jl
@@ -1,11 +1,21 @@
+using Dates, HDF5, Statistics
 
-function _read_h5_by_idx(file, time)
+function _read_h5_by_idx(file::String, time::Dates.DateTime)
     return h5open(file, "r") do file
         return read(file, string(time))
     end
 end
 
-function comp_new_reserve_requirement(min5_flag::Bool, rank_netload::Bool)
+function _extract_fcst_matrix(file::String, time::Dates.DateTime, min5_flag::Bool)
+    matrix = _read_h5_by_idx(file, time)
+    if min5_flag
+        return matrix[:, 1], matrix[:, 2:end]
+    else
+        return matrix[2:end, 1], matrix[2:end, 2:end]
+    end
+end
+
+function comp_new_reserve_requirement(min5_flag::Bool)
     if min5_flag
         ts_dir = "/Users/hanshu/Desktop/Price_formation/Data/generate_fr_KBoot/NYISO_Min5"
     else
@@ -19,55 +29,37 @@ function comp_new_reserve_requirement(min5_flag::Bool, rank_netload::Bool)
         return length(read(file))
     end
 
-    forecast_error = Dict{Dates.DateTime, Matrix{Float64}}()
+    forecast_error = Dict()
     for ix in 1:num_idx
         if min5_flag
             curr_time = initial_time + Minute(5)*(ix - 1)
         else
             curr_time = initial_time + Hour(ix - 1)
         end
-        solar_forecast = _read_h5_by_idx(solar_file, curr_time)
-        wind_forecast = _read_h5_by_idx(wind_file, curr_time)
-        load_forecast = _read_h5_by_idx(load_file, curr_time)
+        base_solar, solar_forecast = _extract_fcst_matrix(solar_file, curr_time, min5_flag)
+        base_wind, wind_forecast = _extract_fcst_matrix(wind_file, curr_time, min5_flag)
+        base_load, load_forecast = _extract_fcst_matrix(load_file, curr_time, min5_flag)
+        base_netload = base_load - base_solar - base_wind
         net_load = load_forecast - solar_forecast - wind_forecast
         net_load_path = sum(net_load, dims=1)
         net_load_rank = sortperm(vec(net_load_path)) # from low to high
         solar_forecast = solar_forecast[:, net_load_rank] # sort by rank
         wind_forecast = wind_forecast[:, net_load_rank]
         load_forecast = load_forecast[:, net_load_rank] 
-        forecast_error[curr_time] = load_forecast - solar_forecast - wind_forecast
+        mid_netload = load_forecast[:,6] - solar_forecast[:, 6] - wind_forecast[:, 6]
+        time_idx = Hour(curr_time)
+        if time_idx not in forecast_error
+            forecast_error[time_idx] = [[base_netload - mid_netload]]
+        else
+            push!(forecast_error[time_idx], [base_netload - mid_netload])
+        end
+    end
+    reserve_requirement = Dict()
+    for (time_idx, error_list) in forecast_error
+        reserve_requirement[time_idx] = mean(error_list)
     end
     return reserve_requirement
 end
 
 
-function reference()
-    solar_data = Dict{Dates.DateTime, Matrix{Float64}}()
-    wind_data = Dict{Dates.DateTime, Matrix{Float64}}()
-    load_data = Dict{Dates.DateTime, Matrix{Float64}}()
-
-    for ix in 1:num_idx
-        if min5_flag
-            curr_time = initial_time + Minute(5)*(ix - 1)
-        else
-            curr_time = initial_time + Hour(ix - 1)
-        end
-        solar_forecast = _read_h5_by_idx(solar_file, curr_time)
-        wind_forecast = _read_h5_by_idx(wind_file, curr_time)
-        load_forecast = _read_h5_by_idx(load_file, curr_time)
-
-        if rank_netload
-            net_load = load_forecast - solar_forecast - wind_forecast
-            net_load_path = sum(net_load, dims=1)
-            net_load_rank = sortperm(vec(net_load_path)) # from low to high
-            solar_forecast = solar_forecast[:, net_load_rank] # sort by rank
-            wind_forecast = wind_forecast[:, net_load_rank]
-            load_forecast = load_forecast[:, net_load_rank] 
-        end
-
-        solar_data[curr_time] = solar_forecast./base_power
-        wind_data[curr_time] = wind_forecast./base_power
-        load_data[curr_time] = load_forecast./base_power
-    end
-    return solar_data, wind_data, load_data
-end
+reserve_req = comp_new_reserve_requirement(false)
