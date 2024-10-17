13cfeb7dac09190358a59cf0f936caa92dee1a71
Han-shu <1051565276@qq.com>
2024-10-17 15:36:35 -0400

    modified:   evaluate/analysis.jl

--- evaluate/analysis.jl
+++ evaluate/analysis.jl
@@ -17,7 +17,7 @@ function get_gen_names_by_type()
             push!(nuclear_gen_names, g)
         end
     end
-    return fast_gen_names, nuclear_gen_names
+    return fast_gen_names, nuclear_gen_names, thermal_gen_names
 end
 
 function extract_LMP(res_dir::AbstractString, POLICY::AbstractString, run_date)::Tuple{Array{Float64}, Array{Float64}}
@@ -66,92 +66,89 @@ function calc_cost_fr_uc_sol(POLICY::String, res_dir::String, run_date::Dates.Da
     GenFuelCosts = 0
     GenIntegerCosts = 0
     Gen_cost_dict = Dict()
+    Gen_profit = DefaultDict{String, Float64}(0)
     for g in thermal_gen_names
         genfuel_cost = variable_cost[g]*sum(uc_sol["Generator Dispatch"][g][1:extract_len]) + fixed_cost[g]*sum(uc_sol["Commitment status"][g][1:extract_len])
         geninteger_cost = startup_cost[g]*sum(uc_sol["Start up"][g][1:extract_len]) + shutdown_cost[g]*sum(uc_sol["Shut down"][g][1:extract_len]) 
         Gen_cost_dict[g] = genfuel_cost + geninteger_cost
         GenFuelCosts += genfuel_cost
         GenIntegerCosts += geninteger_cost
+        Gen_profit[g] -= genfuel_cost + geninteger_cost
     end
     VOLL = 5000
     load_curtailment_penalty = load_curtailment*VOLL
     TotalCosts = GenFuelCosts + GenIntegerCosts + load_curtailment_penalty
-    file_cost = sum(uc_sol["System operator cost"][1:extract_len])
-    Gen_profit = DefaultDict{String, Float64}(0)
-    for key in keys(uc_sol["Generator Profits"])
-        Gen_profit[key] += sum(uc_sol["Generator Profits"][key][1:extract_len])
+    
+    Gen_energy_revenue = Dict()
+    Gen_reserve_revenue = Dict()
+    for key in keys(uc_sol["Energy Revenues"])
+        Gen_energy_revenue[key] = sum(uc_sol["Energy Revenues"][key][1:extract_len]) 
+        Gen_reserve_revenue[key] = sum(uc_sol["Reserve Revenues"][key][1:extract_len]) 
+        Gen_profit[key] += Gen_energy_revenue[key] + Gen_reserve_revenue[key]
     end
     for key in keys(uc_sol["Other Profits"])
-        Gen_profit[key] += sum(uc_sol["Other Profits"][key][1:extract_len])
+        Gen_energy_revenue[key] = sum(uc_sol["Other Profits"][key][1:extract_len])
+        Gen_profit[key] += Gen_energy_revenue[key]
     end
     println("Policy: $(POLICY), Run date: $(run_date)")
-    println("Calculate cost for from file System operator cost: $(file_cost)")
     println("Load curtailment: $(load_curtailment), Wind curtailment: $(wind_curtailment), Solar curtailment: $(solar_curtailment)")
     println("Generation fuel costs: $(GenFuelCosts), Generation integer costs: $(GenIntegerCosts)")
     println("Total cost: $(TotalCosts)")
     ans = OrderedDict("Generation fuel cost" => GenFuelCosts, "Generation integer cost"=> GenIntegerCosts, "Load curtailment penalty" => load_curtailment_penalty, "Total cost" => TotalCosts, 
-                    "Load curtailment" => load_curtailment, "Wind curtailment" => wind_curtailment, "Solar curtailment" => solar_curtailment)
-    return ans, Gen_profit, Gen_cost_dict
+                    "Load curtailment" => load_curtailment, "Generation energy revenue" => sum(values(Gen_energy_revenue)), "Generation reserve revenue" => sum(values(Gen_reserve_revenue)))
+    return ans, Gen_energy_revenue, Gen_reserve_revenue, Gen_cost_dict, Gen_profit
 end
 
 res_dir = "/Users/hanshu/Desktop/Price_formation/Result"
-run_dates = Dict("SB" => Dates.Date(2024,10,2), 
-                "PF" => Dates.Date(2024,10,8),
-                "NR" => Dates.Date(2024,10,4), 
-                "BNR" => Dates.Date(2024,10,4), 
-                "WF" => Dates.Date(2024,10,5), 
-                "DR" => Dates.Date(2024,10,16))
+run_dates = Dict("DR" => Dates.Date(2024,10,17))
+                # "SB" => Dates.Date(2024,10,2), 
+#                 "PF" => Dates.Date(2024,10,8),
+#                 "NR" => Dates.Date(2024,10,4), 
+#                 "BNR" => Dates.Date(2024,10,4), 
+#                 "WF" => Dates.Date(2024,10,5), 
+                
 
 uc_sol = read_json("/Users/hanshu/Desktop/Price_formation/Result/Master_SB/SB_2024-10-02/UC_2019-01-01.json")
 extract_len = length(uc_sol["Time"])
 
 Costs = Dict()
-Gen_profits = Dict()
-Gen_costs = Dict()
+GenEnergyRevenues = Dict()
+GenReserveRevenues = Dict()
+GenProfits = Dict()
+GenCosts = Dict()
 for POLICY in collect(keys(run_dates))
     run_date = run_dates[POLICY]
     file_date = Dates.Date(2019,1,1)
-    ans, gen_profits, gen_costs = calc_cost_fr_uc_sol(POLICY, res_dir, run_date, file_date, extract_len = extract_len)
+    ans, Gen_energy_revenue, Gen_reserve_revenue, Gen_cost_dict, Gen_profit = calc_cost_fr_uc_sol(POLICY, res_dir, run_date, file_date, extract_len = extract_len)
     Costs[POLICY] = ans
-    Gen_profits[POLICY] = gen_profits
-    Gen_costs[POLICY] = gen_costs
+    GenEnergyRevenues[POLICY] = Gen_energy_revenue
+    GenReserveRevenues[POLICY] = Gen_reserve_revenue
+    GenProfits[POLICY] = Gen_profit
+    GenCosts[POLICY] = gen_costs
 end
 
-fast_gen_names, nuclear_gen_names = get_gen_names_by_type()
-fast_gen_profits = Dict(POLICY => sum(Gen_profits[POLICY][g] for g in fast_gen_names) for POLICY in collect(keys(run_dates)))
-nuclear_gen_profits = Dict(POLICY => sum(Gen_profits[POLICY][g] for g in nuclear_gen_names) for POLICY in collect(keys(run_dates)))
-storage_profits = Dict(POLICY => sum(Gen_profits[POLICY][b] for b in ["BA", "PH"]) for POLICY in collect(keys(run_dates)))
-all_gen_profits = Dict(POLICY => sum(Gen_profits[POLICY][g] for g in keys(Gen_profits[POLICY])) for POLICY in collect(keys(run_dates)))
+fast_gen_names, nuclear_gen_names, thermal_gen_names = get_gen_names_by_type()
+
+fast_gen_profits = Dict(POLICY => sum(GenProfits[POLICY][g] for g in fast_gen_names) for POLICY in collect(keys(run_dates)))
+nuclear_gen_profits = Dict(POLICY => sum(GenProfits[POLICY][g] for g in nuclear_gen_names) for POLICY in collect(keys(run_dates)))
+thermal_gen_profits = Dict(POLICY => sum(GenProfits[POLICY][g] for g in thermal_gen_names) for POLICY in collect(keys(run_dates)))
+storage_profits = Dict(POLICY => sum(GenProfits[POLICY][b] for b in ["BA", "PH"]) for POLICY in collect(keys(run_dates)))
+all_gen_profits = Dict(POLICY => sum(GenProfits[POLICY][g] for g in keys(GenProfits[POLICY])) for POLICY in collect(keys(run_dates)))
 
 df = DataFrame(POLICY = collect(keys(run_dates)), 
                 TotalCosts = [Costs[POLICY]["Total cost"] for POLICY in collect(keys(run_dates))],
                 Load_curtailment = [Costs[POLICY]["Load curtailment"] for POLICY in collect(keys(run_dates))],
-                # Wind_curtailment = [Costs[POLICY]["Wind curtailment"] for POLICY in collect(keys(run_dates))],
-                # Solar_curtailment = [Costs[POLICY]["Solar curtailment"] for POLICY in collect(keys(run_dates))],
                 Generation_fuel_cost = [Costs[POLICY]["Generation fuel cost"] for POLICY in collect(keys(run_dates))],
                 Generation_integer_cost = [Costs[POLICY]["Generation integer cost"] for POLICY in collect(keys(run_dates))],
                 Load_curtailment_penalty = [Costs[POLICY]["Load curtailment penalty"] for POLICY in collect(keys(run_dates))],
+                Generation_energy_revenue = [Costs[POLICY]["Generation energy revenue"] for POLICY in collect(keys(run_dates))],
+                Generation_reserve_revenue = [Costs[POLICY]["Generation reserve revenue"] for POLICY in collect(keys(run_dates))],
                 Fast_gen_profits = [fast_gen_profits[POLICY] for POLICY in collect(keys(run_dates))],
                 Nuclear_gen_profits = [nuclear_gen_profits[POLICY] for POLICY in collect(keys(run_dates))],
                 Storage_profits = [storage_profits[POLICY] for POLICY in collect(keys(run_dates))],
                 All_gen_profits = [all_gen_profits[POLICY] for POLICY in collect(keys(run_dates))])
 
-problem_gen = []
-for key in keys(Gen_costs["SB"])
-    if Gen_costs["SB"][key] < 0
-        push!(problem_gen, key)
-    end
-end
-sys = build_ny_system(base_power = 100)
-problem_gen_df = DataFrame(Gen = problem_gen, Startup_cost = [get_start_up(get_operation_cost(get_component(ThermalStandard, sys, g))) for g in problem_gen], 
-                            Fixed_cost = [get_fixed(get_operation_cost(get_component(ThermalGen, sys, g))) for g in problem_gen],
-                            Variable_cost = [get_cost(get_variable(get_operation_cost(get_component(ThermalGen, sys, g)))) for g in problem_gen],
-                            min_Pg = [get_active_power_limits(get_component(ThermalGen, sys, g)).min for g in problem_gen],
-                            max_Pg = [get_active_power_limits(get_component(ThermalGen, sys, g)).max for g in problem_gen],
-)
-
-problem_gen_df.cost_minPg = problem_gen_df.Fixed_cost + problem_gen_df.Variable_cost.*problem_gen_df.min_Pg
-    
+                
 # thermal_gen_names = get_name.(get_components(ThermalGen, sys))
 # fixed_cost = Dict(g => get_fixed(get_operation_cost(get_component(ThermalGen, sys, g))) for g in thermal_gen_names)
 # startup_cost = Dict(g => get_start_up(get_operation_cost(get_component(ThermalStandard, sys, g))) for g in thermal_gen_names)
